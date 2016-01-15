8fa83193a096faaff7b382e017409b9ab9b729d5
Benjamin Lämmle <Benjamin Lmmle@192.168.2.106>
2016-01-15 18:05:42 +0100

    V.1.2.1
    Ich bin gerade dran das mit der verscheibung zu machen.

--- Hausaufgabenheft/src/data/test.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-public class test {
-
-	public String[] day = { "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag" };
-
-	public String[] date = { "17.06", "18.06", "19.06", "20.06", "21.06" };
-}
